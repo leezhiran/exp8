a684a57eca51d9ff673493647f472a14be6f5edd
jonathan1984@126.com <jonathan1984@126.com>
2019-11-25 17:35:03 +0800

    1

--- 实验8/Prim.h
+++ 实验8/Prim.h
@@ -1,3 +1,36 @@
 #pragma once
+#include "iostream"
+#include "algorithm"
+#include "vector"
+#include "queue"
+#include "Graph.h"
 class Prim {
+private:
+	Graph* connectionMap;
+	vector<Edge>edges;
+	vector<Edge>complenmentary;
+	int vertexCount;
+	int edgeCount;
+	int unionedPoint;
+	int chosenPoint;
+	int close;
+public:
+	Prim(int vertexCount, int edgeCount, vector<Edge> edgeInfo) {
+		connectionMap = new Graph(vertexCount, true, edgeInfo);
+		this->edges = vector<Edge>(edgeInfo);
+		this->edgeCount = edgeCount;
+		this->vertexCount = vertexCount;
+		sort(edges.begin(), edges.end(), cmp);
+		chosenPoint = 0;
+	}
+	int find(vector<Edge> complenmentary, vector<Edge> edges){
+
+	}
+	/*生成最小生成树
+	返回值： -1:不存在连通的最小生成树
+			（int）：最小生成树的权值和
+	*/
+	int generateTree(int chosenPoint){
+		 find
+	}
 };
--- 实验8/main.cpp
+++ 实验8/main.cpp
@@ -1,11 +1,12 @@
 #include "iostream"
 #include "Kruskal.h"
+#include "Prim.h"
 using namespace std;
 int main() {
 	vector<Edge> edges;
 	edges.push_back(Edge(0, 1, 1));
 	edges.push_back(Edge(1, 2, 4));
 	edges.push_back(Edge(0, 2, 2));
-	Kruskal k(3,3,edges);
+	Kruskal k(3,3,edges); 
 	k.generateTree();
 }
